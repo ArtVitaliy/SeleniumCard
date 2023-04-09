d421194e4e90891a12cee7f1f77a7b3ffbcd1f0e
ArtVitaly <0331116@mail.ru>
2023-04-09 20:00:35 +0300

    Adding input_invalid to tests

--- src/test/java/ru/netology/selenium/SeleniumCardTest.java
+++ src/test/java/ru/netology/selenium/SeleniumCardTest.java
@@ -52,6 +52,7 @@ public class SeleniumCardTest {
 
     }
 
+
     @Test
     void nameTest() {
         driver.get("http://localhost:9999");
@@ -60,7 +61,7 @@ public class SeleniumCardTest {
         driver.findElement(By.cssSelector("[data-test-id=agreement]")).click();
         driver.findElement(By.cssSelector("button")).click();
         String expected = "Имя и Фамилия указаные неверно. Допустимы только русские буквы, пробелы и дефисы.";
-        String actual = driver.findElement(By.cssSelector("[data-test-id=name] .input__sub")).getText().trim();
+        String actual = driver.findElement(By.cssSelector("[data-test-id='name'].input_invalid .input__sub")).getText().trim();
         assertEquals(expected, actual);
 
     }
@@ -73,7 +74,7 @@ public class SeleniumCardTest {
         driver.findElement(By.cssSelector("[data-test-id=agreement]")).click();
         driver.findElement(By.cssSelector("button")).click();
         String expected = "Телефон указан неверно. Должно быть 11 цифр, например, +79012345678.";
-        String actual = driver.findElement(By.cssSelector("[data-test-id=phone] .input__sub")).getText().trim();
+        String actual = driver.findElement(By.cssSelector("[data-test-id=phone].input_invalid .input__sub")).getText().trim();
         assertEquals(expected, actual);
     }
 
@@ -84,7 +85,7 @@ public class SeleniumCardTest {
         driver.findElement(By.cssSelector("[data-test-id=agreement]")).click();
         driver.findElement(By.cssSelector("button")).click();
         String expected = "Поле обязательно для заполнения";
-        String actual = driver.findElement(By.cssSelector("[data-test-id=phone] .input__sub")).getText().trim();
+        String actual = driver.findElement(By.cssSelector("[data-test-id=phone].input_invalid .input__sub")).getText().trim();
         assertEquals(expected, actual);
     }
 
@@ -97,6 +98,16 @@ public class SeleniumCardTest {
         assertTrue(driver.findElement(By.cssSelector("[data-test-id=agreement].input_invalid")).isDisplayed());
     }
 
+    @Test
+    public void nameEmptyFieldTest() {
+        driver.get("http://localhost:9999");
+        driver.findElement(By.cssSelector("[data-test-id=phone] input")).sendKeys("+79655909090");
+        driver.findElement(By.cssSelector("[data-test-id=agreement]")).click();
+        driver.findElement(By.cssSelector("button")).click();
+        String actual = driver.findElement(By.cssSelector("[data-test-id=name].input_invalid .input__sub")).getText();
+        String expected = "Поле обязательно для заполнения";
+        assertEquals(expected, actual);
+    }
 }
 
 
